389d6fcdc4bb8c43ab4885cf9b3141b78681a1f7
MizuruAndFelix <113217149+MizuruAndFelix@users.noreply.github.com>
2023-07-16 05:21:30 +0300

    Update ConsoleApplication5.cpp

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,12 +1,9 @@
-
 #include <iostream>
 #include <locale.h>
 #include <windows.h>
 #include <string>
 #include<array>
 using namespace std;
-// 2 3 3 2
-// 2 2 0 0
 void printArr(int arr[], int size) {
 	cout << endl;
 	for (int i = 0; i < size; i++) {
@@ -14,39 +11,6 @@ void printArr(int arr[], int size) {
 	}
 	cout << endl;
 }
-//void RemoveArrItem (int arr[], int target, int size) {
-//	int counter = 0;
-//	for (int i = 0; i < size; i++){
-//		if (arr[i] == target) {
-//			arr[i] = 0;
-//			counter++;
-//		}
-//	}
-//	cout << endl << "couter = " << counter << endl;
-//	printArr(arr, size);
-//}
-
-//int NumSet(int arr[], int target, int size) {
-//	int result = 0;
-//	int left = 0;
-//	int right = size;
-//	int middle = (left + right) / 2;
-//
-//	//for (int i = 0; i<size; i++) {
-//		if (arr[middle] = target){
-//			result = middle;
-//		}
-//		else if (arr[middle] > target) {
-//			result = left;
-//		}
-//		else if (arr[middle] < target) {
-//			result = right;
-//		}
-//		return result;
-//	}
-
-
-
 int search(int arr[], int target, int size) {
 	int couter = 0;
 	int NumTarget = 0;
@@ -54,15 +18,14 @@ int search(int arr[], int target, int size) {
 	int right = size;
 	int middle = (left + right) / 2;
 
+	if (arr[left] > target) {
+		return couter = size;
+	}
+
 	if (arr[middle] == target) {
 		NumTarget = middle;// мид
 		
 	}
-
-
-
-
-
 	if (arr[left] <= target) {//лево
 		NumTarget = left;
 
@@ -85,7 +48,6 @@ int search(int arr[], int target, int size) {
 				}
 			}
 		}
-
 
 		right = middle;
 		middle = (left + right) / 2;
@@ -140,25 +102,6 @@ int search(int arr[], int target, int size) {
 		}
 	return couter;
 	}
-
-
-
-	//                                left { 14, 16, 19, 32, 32, 32, 56, 69, 72 } right num = 17
-	//if (arr[NumTarget] != target) { // NumT = 0
-	//	if (NumTarget == left) {  // 0 = 0  t
-	//		while (arr[NumTarget] < target ) { // 14 < 17 t |
-	//			NumTarget++; // 0+1 = 1 | 16 < 17
-	//		}
-	//	}
-	//	if (NumTarget == middle) {
-	//		while (arr[NumTarget] != target) {
-	//			NumTarget++;
-	//		}
-	//	}
-	//}
-
-
-
 
 int main()
 {
